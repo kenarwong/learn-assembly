813f0ac08e222f38c2bfdc2d9878cc230981c949
Ken Hwang <ken.r.hwang.com>
2024-10-23 22:08:31 +0800

    Intro to MIPS: Started Ch6

---
+++ intro-to-mips/ch6/Program6-1.c
@@ -0,0 +1,9 @@
+//main
+//{
+//  static volatile int a = 5;
+//  static volatile int b = 2;
+//  static volatile int c = 3;
+//  int x = prompt("Enter a value for x: ");
+//  int y = a * x * x + b * x + c;
+//  print("The result is: " + y);
+//}
